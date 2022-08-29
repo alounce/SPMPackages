e436168046d19e1695d04169b565afa954abdcff
Oleksandr Karpenko <okarp@softserveinc.com>
2022-08-29 11:15:57 +0300

    fix format in unit tests

--- Core/Tests/CoreHelperTests/StringFormatterTests.swift
+++ Core/Tests/CoreHelperTests/StringFormatterTests.swift
@@ -10,22 +10,14 @@ import XCTest
 
 class StringFormatterTests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
     func testStringFromMoney() throws {
         let string = StringFormatter.stringFromMoney(22)
-        XCTAssertEqual(string, "22.00 $")
+        XCTAssertEqual(string, "$22.00")
     }
     
     func testStringFromMoneyNegative() throws {
         let string = StringFormatter.stringFromMoney(-22)
-        XCTAssertEqual(string, "-22.00 $")
+        XCTAssertEqual(string, "-$22.00")
     }
 
 }
--- Core/Tests/NetworkingTests/EndpointTests.swift
+++ Core/Tests/NetworkingTests/EndpointTests.swift
@@ -10,14 +10,6 @@ import XCTest
 
 class EndpointTests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
     func testInitialier() throws {
         let expectedBaseURL = "https://my-json-server.typicode.com/alounce/demo"
         let expectedPath = "products"
